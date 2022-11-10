5afc381315aa46e5a7cb255aa1fa94312b008afc
Listopaddon <67251998+Listopaddon@users.noreply.github.com>
2022-11-10 18:57:17 +0300

    Add project files.

---
+++ Lesson_11/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public enum Operation
+{
+    Sum,
+    Subtract,
+    Multiply
+}
+
+public class OperationManager
+{
+    private int _first;
+    private int _second;
+    ExecutionManager manager;
+
+
+    public OperationManager(int first, int second)
+    {
+        _first = first;
+        _second = second;
+        manager = new ExecutionManager();
+    }
+
+    public int Execute(Operation operation)
+    {
+        return manager.FuncExecute[operation].Invoke(_first, _second);
+    }
+}
+
+//Implement functionality
+public class ExecutionManager
+{
+    public Dictionary<Operation, Func<int, int, int>> FuncExecute { get; set; }
+    //Add delegates for sum, substrat and multiply here
+    public ExecutionManager()
+    {
+        FuncExecute = new Dictionary<Operation, Func<int, int, int>>();
+        PrepareExecution();
+    }
+
+    public void PrepareExecution()
+    {
+        FuncExecute.Add(Operation.Multiply, (a, b) => a * b);
+        FuncExecute.Add(Operation.Subtract, (a, b) => a - b);
+        FuncExecute.Add(Operation.Sum, (a, b) => a + b);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var opManager = new OperationManager(20, 10);
+        var result = opManager.Execute(Operation.Subtract);
+        Console.WriteLine($"The result of the operation is {result}");
+        Console.ReadKey();
+    }
+}
